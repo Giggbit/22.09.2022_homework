c00a87b9a42b0b15ac6797abe9c229042ca0181c
Giggbit <danya19981017@gmail.com>
2022-09-22 11:02:12 +0200

    Добавьте файлы проекта.

---
+++ 22.09.2022 Семантика переноса.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "MyString.h"
+using namespace std;
+
+int main() {
+	/*MyString obj("Daniil");
+	MyString obj2 = move(obj);
+	obj2.Print();*/
+
+	MyString obj3{'S','k','o','d','a'};
+	obj3.Print();
+}
+
